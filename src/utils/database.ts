15b9a14fb071b7e61b4e016b225f647b25b1f224
Kian Wie Lim <keaneplim@gmail.com>
2025-09-04 16:20:45 +0700

    changing the format7

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -755,7 +755,7 @@ function printNameTagAndroidFallback(student: Student, category: string) {
           /* Print styles - Simplified for Android compatibility */
           @media print {
             body {
-              margin-top: 44mm;
+              margin-top: 40mm;
               padding: 0;
               background: white;
               font-family: Poppins, sans-serif;
